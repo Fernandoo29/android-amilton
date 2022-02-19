e7f021e57791e567107ff8b94a803aa968f3dc83
Fernandoo29 <86057233+Fernandoo29@users.noreply.github.com>
2022-02-18 22:54:53 -0300

    trans pasta
    pasta trans

---
+++ teste.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = "Loafa";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "formulara";
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+?>
